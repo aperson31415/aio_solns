675b35d14107cf7b9791e930ff5380721eac3700
Human <152773360+aperson31415@users.noreply.github.com>
2025-06-02 21:36:31 +0800

    Update mixedfraction.py

--- starter problems/mixedfraction.py
+++ starter problems/mixedfraction.py
@@ -1,13 +1,12 @@
-rawinput = open('mixin.txt', 'r').read()
-n, d = rawinput.split()
-n, d = int(n), int(d)
+n, d = map(int, input().split())
  
-b = n % d
-a = int((n-b)/d)
- 
-output_file = open('mixout.txt', 'w')
+b = n % d # The numerator part (proper fraction)
+a = int((n-b)/d) # The whole part
+
+
 if b == 0:
     output_file.write(f'{a}')
 else:
     output_file.write(f'{a} {b}/{d}')
-output_file.close()
+
+print(a if b == 0 else f'{a} {b}/{d}') # a b/d if b != 0, or just a if b == 0
