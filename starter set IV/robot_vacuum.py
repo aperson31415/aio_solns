2136ebacda8678f29f5091b7a132e44948202fd7
Human <152773360+aperson31415@users.noreply.github.com>
2025-06-03 06:51:34 +0800

    Create robot_vacuum.py

---
+++ starter set IV/robot_vacuum.py
@@ -0,0 +1,12 @@
+K = int(input())
+I = input()
+ 
+x = 0
+y = 0
+for i in I:
+    if i == 'N': y += 1
+    elif i == 'E': x += 1
+    elif i == 'S': y -= 1
+    elif i == 'W': x -= 1
+
+print(abs(x)+abs(y))
