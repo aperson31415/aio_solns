8aea2c1470858844ce48d87cf1926aed3bee9fe6
Human <152773360+aperson31415@users.noreply.github.com>
2025-06-03 06:44:35 +0800

    Create art_clas_2.py

---
+++ starter set II/art_clas_2.py
@@ -0,0 +1,16 @@
+N = int(rawinput[0])
+H = [int(input()) for _ in range(N)]
+ 
+maxx = 0
+minx = 100001
+maxy = 0
+miny = 100001
+ 
+for i in H:
+    x, y = map(int, i.split())
+    if x > maxx: maxx = x
+    if x < minx: minx = x
+    if y > maxy: maxy = y
+    if y < miny: miny = y
+ 
+print((maxx-minx)*(maxy-miny))
