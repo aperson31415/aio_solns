dc58393278a3eb0f8f24b2c00bccc7361da03c55
Human <152773360+aperson31415@users.noreply.github.com>
2026-01-30 20:54:08 +0800

    Delete starter set II directory

--- starter set II/culture.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
- 
-i = 0
-N = n
-while N%2 == 0:
-    i += 1
-    N /= 2
-
-print(f'{n} {i}')
--- starter set II/ladybugs.py
+++
@@ -1,6 +0,0 @@
-N = int(input())
-F = [int(input()) for _ in range(N)]
- 
-output = max(F)-min(F)+1
- 
-print(output)
