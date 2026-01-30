f86d41fa1bab44194c8c2954c81acc5eab485c2b
Human <152773360+aperson31415@users.noreply.github.com>
2026-01-30 20:53:55 +0800

    Delete starter problems directory

--- starter problems/addition.py
+++
@@ -1,2 +0,0 @@
-a, b = map(int, input().split()) # Get a & b
-print(a+b) # Added them up
--- starter problems/count_to_infinity.py
+++
@@ -1,3 +0,0 @@
-number = int(input())
-for i in range(number):
-    print(str(i + 1)) # i+1 because indexes start at 0
